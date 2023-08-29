af6dbb5b6b3cbb4e3b7826b97b9b3ff494c14f87
Ajay Sharma <ajaysh73@gmail.com>
2023-08-29 15:58:11 -0400

    Worked on menu component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import Menu from './Menu';
+import Link from 'next/link';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='h-12 items-center text-red-500 p-4 flex justify-between border-b-2 border-b-red-500 uppercase '>
+      {/* //LOGO */}
+      <div className='text-xl'>
+        <Link href='/'>ANTONIO'S</Link>
+      </div>
+      {/* //MOBILE MENU */}
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
