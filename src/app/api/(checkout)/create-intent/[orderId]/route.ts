edb629bdad80ba2dc41850b55b48d9b65b888413
Ajay Sharma <ajaysh73@gmail.com>
2023-09-15 15:46:28 -0400

    fixed order can be null

--- src/app/api/(checkout)/create-intent/[orderId]/route.ts
+++ src/app/api/(checkout)/create-intent/[orderId]/route.ts
@@ -2,40 +2,39 @@ import { OrderType } from '@/app/types/types';
 import { prisma } from '@/utils/connect';
 import { NextRequest, NextResponse } from 'next/server';
 
-
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 export const POST = async (
   request: NextRequest,
   { params }: { params: { orderId: string } }
 ) => {
   const { orderId } = params;
-  const order: OrderType = await prisma.order.findUnique({
+  const order: OrderType | null = await prisma.order.findUnique({
     where: {
       id: orderId,
     },
   });
-console.log(order)
-if (order) {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: order.price * 100,
-    currency: "usd",
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-console.log(paymentIntent)
-  await prisma.order.update({
-    where: {
-      id: orderId,
-    },
-    data: { intent_id: paymentIntent.id },
-  });
+  console.log(order);
+  if (order) {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: order.price * 100,
+      currency: 'usd',
+      automatic_payment_methods: {
+        enabled: true,
+      },
+    });
+    console.log(paymentIntent);
+    await prisma.order.update({
+      where: {
+        id: orderId,
+      },
+      data: { intent_id: paymentIntent.id },
+    });
 
-  return new NextResponse(
-    JSON.stringify({ clientSecret: paymentIntent.client_secret }),
-    { status: 200 }
-  );
-}
+    return new NextResponse(
+      JSON.stringify({ clientSecret: paymentIntent.client_secret }),
+      { status: 200 }
+    );
+  }
 
   return new NextResponse(JSON.stringify({ message: 'Order not found!.' }), {
     status: 404,
